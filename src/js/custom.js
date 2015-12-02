8fa22416b93187030c45bdebfd28274443ac15b1
Denis Kulikov <altingfest@gmail.com>
2015-12-02 12:18:16 +0300

    First commit

---
+++ src/js/custom.js
@@ -0,0 +1,17 @@
+/**************************************/
+/* Custom JavaScript files supervisor */
+/**************************************/
+
+$(document).ready(function() {
+
+    /* Custom */
+
+ /*    //= ./common/material-init.js */
+ /*    //= ./common/google-analytics.js */
+  $(".mbr-main-anim").height(window.innerHeight);
+  $("#mbr-clients-slider").owlCarousel({
+    loop: true,
+    items: 4
+  });
+
+});
